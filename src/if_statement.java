74898d2bedfef551ea8a76546d1c8245164474be
JethroBalaga <balagajet@gmail.com>
2023-10-10 07:08:36 +0800

    added initial files

---
+++ src/if_statement.java
@@ -0,0 +1,16 @@
+
+public class if_statement {
+    public static void main(String[]args){
+        int time = 19;
+        if(time < 10) {
+            System.out.println("good morning");
+        }
+        else if (time > 18){
+            System.out.println("good day");
+        } 
+        else {
+            System.out.println("good evening");
+        }
+    }
+}
+    //keep it mind sa mga braces :>
